ed23c30c8c2daf79ece9bf279beaf18f6c1410b9
shubhmmmgit <shubhamjha2003@gmail.com>
2026-02-17 14:42:05 +0530

    FETCHING ERROR FOR IOS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,13 @@ import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
+window.onerror = function (msg, url, line) {
+  alert("ERROR: " + msg + " at line " + line);
+};
+
+window.onunhandledrejection = function (e) {
+  alert("PROMISE ERROR: " + e.reason);
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
