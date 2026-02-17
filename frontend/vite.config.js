a7c772a7ab5836760ac53e54e4972222fe76fb15
shubhmmmgit <shubhamjha2003@gmail.com>
2026-02-17 11:39:23 +0530

    FIX SAFARI COMPATIBILITY

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    target: ['es2015', 'safari11'],
     outDir: 'dist' 
   }
 })
